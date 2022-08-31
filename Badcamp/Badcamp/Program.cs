02823c74cb80972bb25df697117062f0b73d45a8
CaineCorso <danielz.boros@gmail.com>
2022-08-31 17:19:53 +0200

    modify program.cs for dummy storage

--- Badcamp/Badcamp/Program.cs
+++ Badcamp/Badcamp/Program.cs
@@ -1,3 +1,6 @@
+using Badcamp.Services;
+using Badcamp.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISongStorage, SongStorage>();
 
 var app = builder.Build();
 
