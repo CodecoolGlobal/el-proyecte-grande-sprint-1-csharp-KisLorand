98f1ef3f19f1ab2b9ac6f8ed549e00aab0532447
mBartal88 <miklos.bartal@gmail.com>
2022-08-31 13:57:29 +0200

    adding user model with constructor

---
+++ Badcamp/Badcamp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Badcamp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+    }
+}
