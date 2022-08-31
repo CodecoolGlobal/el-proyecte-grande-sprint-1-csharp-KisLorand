7d8e32e9c3f6f780d70198b4fa86ac617edc804c
CaineCorso <danielz.boros@gmail.com>
2022-08-31 17:18:16 +0200

    create song model

--- Badcamp/Badcamp/Models/Song.cs
+++ Badcamp/Badcamp/Models/Song.cs
@@ -2,5 +2,21 @@
 {
     public class Song
     {
+        private Guid _songID;
+        private Guid _artistID;
+        private string _title;
+        private string _source;
+
+        public Guid SongID => _songID;
+        public Guid ArtistID => _artistID;
+        public string Title => _title;
+        public string Source => _source;
+        public Song()
+        {
+            _songID = Guid.NewGuid();
+            _artistID = Guid.NewGuid();
+            _title = "1234";
+            _source = "ghfdvjzck";
+        }
     }
 }
