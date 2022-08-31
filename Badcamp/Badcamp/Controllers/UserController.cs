cc8935ea16ee8187b2e5b3e9ad37ed70dc1e6e8e
mBartal88 <miklos.bartal@gmail.com>
2022-08-31 15:10:07 +0200

    adding endpoints to user controller

--- Badcamp/Badcamp/Controllers/UserController.cs
+++ Badcamp/Badcamp/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Badcamp.Services;
+﻿using Badcamp.Models;
+using Badcamp.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,36 @@ namespace Badcamp.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        
+        private UserStorage _userStorage;
+
+        public UserController(UserStorage userStorage)
+        {
+            _userStorage = userStorage;
+        }
+
+        [HttpGet("/GetUsers")]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _userStorage.GetUsers();
+        }
+
+        [HttpPost("/RegisterUser")]
+        public void RegisterUser([FromBody] User newUser)
+        {
+            _userStorage.AddUser(newUser);
+        }
+
+        [HttpPut("/UpdatePassword/{userName}")]
+        public void UpdateUser([FromRoute] string userName, [FromBody] User user)
+        {
+            _userStorage.UpdateUserPassword(userName, user.Password);
+        }
+
+
+
+
+
+
+
     }
 }
