dd583e2760470ebf3197f57b1b4c7438db65ef5b
mBartal88 <miklos.bartal@gmail.com>
2022-08-31 14:06:49 +0200

    adding dummy user storage

---
+++ Badcamp/Badcamp/Services/UserStorage.cs
@@ -0,0 +1,15 @@
+﻿using Badcamp.Models;
+
+namespace Badcamp.Services
+{
+    public class UserStorage
+    {
+        private List<User> _users;
+
+        public UserStorage()
+        {
+            _users = new List<User>();
+        }
+
+    }
+}
