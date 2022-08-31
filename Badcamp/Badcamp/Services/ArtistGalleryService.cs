c5058cbc7e03b802c7ec89a2ff133de5c1253e16
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-08-31 15:28:26 +0200

    create ArtistGalleryService.cs

--- Badcamp/Badcamp/Services/ArtistGalleryService.cs
+++ Badcamp/Badcamp/Services/ArtistGalleryService.cs
@@ -2,5 +2,11 @@
 {
     public class ArtistGalleryService
     {
+        private ArtistStorage _artistStorage;
+
+        public ArtistGalleryService(ArtistStorage artistStorage)
+        {
+            _artistStorage = artistStorage;
+        }
     }
 }
