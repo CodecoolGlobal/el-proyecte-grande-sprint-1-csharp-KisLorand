1fa20e91d1f7a5537dec3608fd0b58e219225204
CaineCorso <danielz.boros@gmail.com>
2022-08-31 17:21:18 +0200

    add GetSong and GetAllSongs endpoint

--- Badcamp/Badcamp/Controllers/SongController.cs
+++ Badcamp/Badcamp/Controllers/SongController.cs
@@ -1,15 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Badcamp.Models;
+using Badcamp.Services;
+using Badcamp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Badcamp.Controllers
 {
     [ApiController]
     public class SongController : ControllerBase
     {
-        [HttpGet]
-        [Route("/api/[controller]/echotest")]
-        public ActionResult<string> EchoTest()
+        private readonly ISongStorage _storage;
+
+        public SongController(ISongStorage songStorage)
         {
-            return Ok("initial echo test.");
+            _storage = songStorage;
+        }
+
+        [HttpGet]
+        [Route("{songID}")]
+        public ActionResult<Song> GetSong([FromRoute] Guid songID)
+        {
+            Song song = _storage.GetSong(songID);
+            return Ok(song);
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public ActionResult<List<Song>> GetAll()
+        {
+            List<Song>? response = _storage.GetAllSongs();
+            return Ok(response);
         }
     }
 }
