9430e94215808051e6771b42d827b99478177f16
KisLorand <kislorand270@gmail.com>
2022-09-01 09:46:28 +0200

    add Update to ArtistPageService

--- Badcamp/Badcamp/Services/ArtistPageService.cs
+++ Badcamp/Badcamp/Services/ArtistPageService.cs
@@ -13,7 +13,7 @@ namespace Badcamp.Services
 		}
 
 		public ArtistModel GetOne(int id)
-		{ 
+		{
 			return _artistStorage.GetArtist(id);
 		}
 
@@ -26,6 +26,16 @@ namespace Badcamp.Services
 		{
 			_artistStorage.AddArtist(artist);
 			return artist;
+		}
+
+		public ArtistModel Update(int id, [FromBody] ArtistModel artist)
+		{
+			ArtistModel updatedArtist = _artistStorage.GetArtist(id);
+			updatedArtist.Name = artist.Name;
+			updatedArtist.Description = artist.Description;
+			updatedArtist.ArtistGenre = artist.ArtistGenre;
+			updatedArtist.ProfilePicture = artist.ProfilePicture;
+			return updatedArtist;
 		}
 	}
 }
