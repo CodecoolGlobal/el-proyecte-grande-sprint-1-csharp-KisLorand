c8902c1ca4416450323ad78fbbba7f51a05cf4fd
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-01 12:42:01 +0200

    add GetAllArtist() to ArtistGalleryController.cs

--- Badcamp/Badcamp/Controllers/ArtistGalleryController.cs
+++ Badcamp/Badcamp/Controllers/ArtistGalleryController.cs
@@ -1,7 +1,26 @@
-﻿namespace Badcamp.Controllers
+﻿using Badcamp.Models;
+using Badcamp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Badcamp.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ArtistGalleryController
     {
 
+        private ArtistGalleryService _artistGalleryService;
+        public ArtistGalleryController(ArtistGalleryService artistGalleryService)
+        {
+            _artistGalleryService = artistGalleryService;
+        }
+
+        [HttpGet("/GetArtists")]
+        public IList<ArtistModel> GetAllArtist()
+        {
+            return _artistGalleryService.GetAllArtists();
+        }
+
+ 
     }
 }
