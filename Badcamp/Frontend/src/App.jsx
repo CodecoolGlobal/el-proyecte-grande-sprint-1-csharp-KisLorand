2e9ff640e606477afbf70d9cacfd2c2871018026
mBartal88 <miklos.bartal@gmail.com>
2022-09-13 13:09:42 +0200

    adding layout component to app

--- Badcamp/Frontend/src/App.jsx
+++ Badcamp/Frontend/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Layout from './components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout />
+        </div>
+    );
 }
 
 export default App;
