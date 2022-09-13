69e0ee96c9c0ab1c23bda462bb6b12517abbcbd1
Imreszab <szabimi12@gmail.com>
2022-09-13 14:10:28 +0200

    add /events route to index.jsx

--- Badcamp/Frontend/src/index.jsx
+++ Badcamp/Frontend/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './components/Home';
+import EventPage from './components/eventComponents/EventPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +13,7 @@ root.render(
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Home />}></Route>
-
+          <Route exact path='/events' element={<EventPage />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
