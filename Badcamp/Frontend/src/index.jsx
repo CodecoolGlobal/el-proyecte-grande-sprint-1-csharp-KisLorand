c45dfdcbc7c504426b78fe694e877111b48ceb02
KisLorand <kislorand270@gmail.com>
2022-09-13 11:28:03 +0200

    add import from Home Component

--- Badcamp/Frontend/src/index.jsx
+++ Badcamp/Frontend/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +12,7 @@ root.render(
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Home />}></Route>
-          
+
         </Route>
       </Routes>
     </BrowserRouter>
