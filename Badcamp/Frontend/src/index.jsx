843b0e7f0c237d554c95ca8bd04a53b1a3b18182
KisLorand <kislorand270@gmail.com>
2022-09-13 11:12:45 +0200

    add base routing

--- Badcamp/Frontend/src/index.jsx
+++ Badcamp/Frontend/src/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Home />}></Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
