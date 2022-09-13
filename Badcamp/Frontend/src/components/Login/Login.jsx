da6a2998cfd910207edec07a5e748d69a32ccbd2
mBartal88 <miklos.bartal@gmail.com>
2022-09-13 15:14:28 +0200

    updating stylesheet

--- Badcamp/Frontend/src/components/Login/Login.jsx
+++ Badcamp/Frontend/src/components/Login/Login.jsx
@@ -1,12 +1,22 @@
+import './Login.css';
+
 const Login = () => {
 
     return (
         <div className="Login">
-            <h1>Login</h1>
-            <input
-                className="input"
-                type="text"
-            />
+            <h1>Login</h1><br /><br />
+            <form>
+                <label>Username</label><br/>
+                <input
+                    className="input"
+                    type="text"
+                /><br /><br />
+                <label>Password</label><br />
+                <input
+                    className="input"
+                    type="text"
+                />
+            </form>
         </div>
     );
 }
