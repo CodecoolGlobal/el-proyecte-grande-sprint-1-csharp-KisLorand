ff6f791dc572855c8f8d3af0125d596ef4277edf
Imreszab <szabimi12@gmail.com>
2022-09-13 14:38:46 +0200

    add ul and mock data to EventPage.jsx

--- Badcamp/Frontend/src/components/eventComponents/EventPage.jsx
+++ Badcamp/Frontend/src/components/eventComponents/EventPage.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import Event from './Event'
+
+const [items, setItems] = useState([
+    {
+    "id": 0,
+    "artistId": 0,
+    "title": "new Song",
+    "discription": "Hey! I released a new song!! Check it out "
+    },
+    {
+    "id": 1,
+    "artistId": 0,
+    "title": "Concert",
+    "discription": "Come listen to us on Friday! see you there!"
+    }
+    ])
 
 const EventPage = () => {
   return (
     <div>
-      
+      <ul>
+        {items.map((item) => {
+            return(
+                <Event 
+                    key={item.id}
+                    item={item}/>
+            )
+        })}
+      </ul>
     </div>
   )
 }
