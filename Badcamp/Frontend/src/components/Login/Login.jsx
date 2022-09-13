71929d962215796825502c5758f9b50748bcb0d8
mBartal88 <miklos.bartal@gmail.com>
2022-09-13 14:43:46 +0200

    adding login component and importing to index component

---
+++ Badcamp/Frontend/src/components/Login/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+
+    return (
+        <div className="Login">
+            <h1>Login</h1>
+            <input
+                className="input"
+                type="text"
+            />
+        </div>
+    );
+}
+
+export default Login;
--- Badcamp/Frontend/src/index.jsx
+++ Badcamp/Frontend/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './components/Home';
+import Login from './components/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
 
                 </Route>
             </Routes>
