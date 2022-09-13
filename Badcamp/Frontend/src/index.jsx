0fb76ae3ee179301ac8af6f370d5a885b6bfa550
KisLorand <kislorand270@gmail.com>
2022-09-13 15:08:20 +0200

    modify index.jsx

--- Badcamp/Frontend/src/index.jsx
+++ Badcamp/Frontend/src/index.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Home from './Home';
+import Home from './components/Home';
+import ArtistPage from './components/artistPage/ArtistPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +13,7 @@ root.render(
       <Routes>
         <Route path='/' element={<App />}>
           <Route index element={<Home />}></Route>
-
+          
         </Route>
       </Routes>
     </BrowserRouter>
