71a1ee3aa626e4c40acf3e359ef91c42fdf92713
KisLorand <kislorand270@gmail.com>
2022-09-14 10:51:46 +0200

    add context parameter to apiRequest

--- Badcamp/Frontend/src/requests/apiRequest.js
+++ Badcamp/Frontend/src/requests/apiRequest.js
@@ -1,12 +1,12 @@
 import { RequestContext } from "./requestContext";
 
-const apiRequest = async (url='', optionsObj=null, errMsg=null) => {
+const apiRequest = async (url='', optionsObj=null, errMsg=null, context=RequestContext) => {
     try {
         const response = await fetch(url, optionsObj);
         if (!response.ok) throw Error('Error occoured. Reload the app');
         if (optionsObj === null) {
             const data = response.json;
-            RequestContext(data);
+            context(data);
         }
     } catch (error) {
         errMsg = error.message;
