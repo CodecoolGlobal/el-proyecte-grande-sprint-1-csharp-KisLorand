5ed1a671a07293c5d2b436a92666d3d20526117d
KisLorand <kislorand270@gmail.com>
2022-09-14 08:34:41 +0200

    add apiRequestGet

--- Badcamp/Frontend/src/apiRequest.js
+++ Badcamp/Frontend/src/apiRequest.js
@@ -1,7 +1,22 @@
+import { RequestContext } from "./requestContext";
+
 const apiRequest = async (url='', optionsObj=null, errMsg=null) => {
     try {
         const response = await fetch(url, optionsObj);
         if (!response.ok) throw Error('Error occoured. Reload the app');
+    } catch (error) {
+        errMsg = error.message;
+    } finally {
+        return errMsg;
+    }
+};
+
+export const apiRequestGet = async (url='', optionsObj=null, errMsg=null) => {
+    try {
+        const response = await fetch(url, optionsObj);
+        if (!response.ok) throw Error('Error occoured. Reload the app');
+        const data = response.json;
+        RequestContext(data);
     } catch (error) {
         errMsg = error.message;
     } finally {
