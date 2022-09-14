dca2e5f10a23c984866b75c59cf44d39e1546bd6
Imreszab <szabimi12@gmail.com>
2022-09-14 10:32:55 +0200

    fix typo

--- Badcamp/Frontend/src/components/eventComponents/EventPage.jsx
+++ Badcamp/Frontend/src/components/eventComponents/EventPage.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import { useState } from 'react'
 import Event from './Event'
 
-const [items, setItems] = useState([
-    {
-    "id": 0,
-    "artistId": 0,
-    "title": "new Song",
-    "discription": "Hey! I released a new song!! Check it out "
-    },
-    {
-    "id": 1,
-    "artistId": 0,
-    "title": "Concert",
-    "discription": "Come listen to us on Friday! see you there!"
-    }
-    ])
+
 
 const EventPage = () => {
+    const [items, setItems] = useState([
+        {
+        "id": 0,
+        "artistId": 0,
+        "title": "new Song",
+        "description": "Hey! I released a new song!! Check it out "
+        },
+        {
+        "id": 1,
+        "artistId": 0,
+        "title": "Concert",
+        "description": "Come listen to us on Friday! see you there!"
+        }
+        ])
   return (
     <div>
-      <ul>
+      
         {items.map((item) => {
             return(
                 <Event 
@@ -28,7 +29,7 @@ const EventPage = () => {
                     item={item}/>
             )
         })}
-      </ul>
+      
     </div>
   )
 }
