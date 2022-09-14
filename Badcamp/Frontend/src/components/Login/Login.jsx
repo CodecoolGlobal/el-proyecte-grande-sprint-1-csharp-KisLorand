83db4bc6dc51833f532d8e1845f0df30329ef5cf
mBartal88 <miklos.bartal@gmail.com>
2022-09-14 10:37:57 +0200

    implementing logic for simple login check

--- Badcamp/Frontend/src/components/Login/Login.jsx
+++ Badcamp/Frontend/src/components/Login/Login.jsx
@@ -1,24 +1,64 @@
 import './Login.css';
+import LoginForm from './LoginForm';
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [error, setError] = useState(null);
+    const redirect = useNavigate();
+
+    const userDatabase = [
+        {
+            id: 1,
+            username: "test",
+            password: "test"
+        },
+        {
+            id: 2,
+            username: "someone",
+            password: "pw"
+        },
+        {
+            id: 3,
+            username: "someone else",
+            password: "123"
+        }
+    ];
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    var { username, password } = document.forms[0];
+    var userData = userDatabase.find((user) => user.username === username.value);
+
+    if (userData) {
+        if (userData.password !== password.value) {
+            setError('Invalid password');
+        } else {
+            setIsLoggedIn(true);
+            redirect('/');
+        }
+    } else {
+        setError('Invalid username');
+    }
+
+}
 
     return (
         <div className="Login">
             <h1>Login</h1><br /><br />
-            <form>
-                <label>Username</label><br/>
-                <input
-                    className="input"
-                    type="text"
-                /><br /><br />
-                <label>Password</label><br />
-                <input
-                    className="input"
-                    type="text"
-                />
-            </form>
+            <LoginForm
+                handleSubmit={handleSubmit}
+                errorMessage={error}
+
+            />
         </div>
     );
 }
 
 export default Login;
+
