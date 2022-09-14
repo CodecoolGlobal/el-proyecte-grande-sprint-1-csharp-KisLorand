c13b7641ed0966dd328551c1752781882510f8f4
KisLorand <kislorand270@gmail.com>
2022-09-14 09:11:00 +0200

    merge two apiRequest functions

--- Badcamp/Frontend/src/apiRequest.js
+++ Badcamp/Frontend/src/apiRequest.js
@@ -4,19 +4,10 @@ const apiRequest = async (url='', optionsObj=null, errMsg=null) => {
     try {
         const response = await fetch(url, optionsObj);
         if (!response.ok) throw Error('Error occoured. Reload the app');
-    } catch (error) {
-        errMsg = error.message;
-    } finally {
-        return errMsg;
-    }
-};
-
-export const apiRequestGet = async (url='', optionsObj=null, errMsg=null) => {
-    try {
-        const response = await fetch(url, optionsObj);
-        if (!response.ok) throw Error('Error occoured. Reload the app');
-        const data = response.json;
-        RequestContext(data);
+        if (optionsObj === null) {
+            const data = response.json;
+            RequestContext(data);
+        }
     } catch (error) {
         errMsg = error.message;
     } finally {
