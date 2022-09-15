4fd44f2f0abd18e43bb26c41c04587d7bcb8e858
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-15 09:36:41 +0200

    add ArtistListing.jsx

--- Badcamp/Frontend/src/components/artistListing/ArtistListing.jsx
+++ Badcamp/Frontend/src/components/artistListing/ArtistListing.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import ALContainer from "./ALContainer";
 
 const ArtistListing = () => {
-  return (
-    <div>kuki</div>
-  )
-}
+  const [searchValue, setSearchValue] = useState("");
+  const [artists, setArtist] = useState([
+    {
+      id: 0,
+      userId: 0,
+      name: "Zoli",
+      description: "Nagyon ugyes vagyok köszönöm szépen ahh aha hahha...",
+      profilePicture: "/path/placeholder/picture.png",
+      genres: ["alter", "techno"],
+    },
+    {
+      id: 1,
+      userId: 0,
+      name: "Laci",
+      description: "Nagyon ugyes vagyok köszönöm szépen ahh aha hahha...",
+      profilePicture: "/path/placeholder/picture.png",
+      genres: ["pop", "rock", "r&b"],
+    },
+    {
+      id: 2,
+      userId: 0,
+      name: "Zolesz",
+      description: "Nagyon ugyes vagyok köszönöm szépen ahh aha hahha...",
+      profilePicture: "/path/placeholder/picture.png",
+      genres: ["alter", "techno"],
+    },
+    {
+      id: 4,
+      userId: 0,
+      name: "Lacika",
+      description: "Nagyon ugyes vagyok köszönöm szépen ahh aha hahha...",
+      profilePicture: "/path/placeholder/picture.png",
+      genres: ["pop", "rock", "r&b"],
+    },
+  ]);
 
-export default ArtistListing
+  return (
+    <div>
+      <ALContainer
+        artists={artists.filter((artist) =>
+          artist.name.toLowerCase().includes(searchValue.toLocaleLowerCase())
+        )}
+        setArtists={setArtist}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+    </div>
+  );
+};
+
+export default ArtistListing;
