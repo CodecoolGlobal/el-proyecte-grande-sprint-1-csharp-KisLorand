920a80d7105779e3266ae3bc9117146e373be6d8
KisLorand <kislorand270@gmail.com>
2022-09-15 13:32:41 +0200

    fix fetching with toggle button

--- Badcamp/Frontend/src/components/artistPage/ArtistPage.jsx
+++ Badcamp/Frontend/src/components/artistPage/ArtistPage.jsx
@@ -2,25 +2,46 @@ import React from 'react';
 import { useState, useEffect, useContext } from 'react';
 import ArtistName from './ArtistName';
 import ArtistDescription from './ArtistDescription';
+import ArtistPicture from './ArtistPicture';
+import ItemList from './ItemList';
+import ToggleButton from './ToggleButton';
 import apiRequest from '../../requests/apiRequest';
 import { RequestContext } from '../../requests/requestContext';
-import ArtistPicture from './ArtistPicture';
 
 const ArtistPage = (props) => {
   const [artist, setArtist] = useState(null); 
-  const url = "http://localhost:3000/artist";
+  const [songs, setSongs] = useState(null); 
+  const [selectSong, setSelectSong] = useState(true);
+
+  const urlArtists = "http://localhost:3000/artist";
+  const urlSongs = "http://localhost:3000/songs";
+  const urlEvents = "http://localhost:3000/events";
+  
   useEffect(() => {
-    apiRequest(url, [artist, setArtist]);
+    apiRequest(urlArtists, [artist, setArtist]);
+    apiRequest(urlSongs, [songs, setSongs])
   }, [props.artistId]);
-  //const {asd, setAsd} = useContext(RequestContext);
-  return (
-    artist === null ? 
-    <p>"Loading..."</p> : 
-    <div>
+
+  useEffect(() => {
+    const url = (selectSong ? urlSongs : urlEvents)
+    apiRequest(url, [songs, setSongs]);
+  }, [selectSong]);
+
+  if (artist === null || songs == null) {
+    return (
+      <p>"Loading..."</p>
+    );
+  } else {
+    return (
+      <div>
         <ArtistName artistName={artist[0].name}  />
+        <ArtistPicture artistProfilePicture={artist[0].profilePicture} height={250} />
         <ArtistDescription artistDesc={artist[0].description}/>
+        <ToggleButton toggle={[selectSong, setSelectSong]}/>
+        <ItemList items={songs} />
     </div>
-  )
+    );
+  };
 };
 
 export default ArtistPage;
