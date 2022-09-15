27aee6cd9191cb30be95809f311c088bbfd5ba01
CaineCorso <danielz.boros@gmail.com>
2022-09-15 13:44:51 +0200

    feat: api request import

--- Badcamp/Frontend/src/components/songComponents/SongListing.jsx
+++ Badcamp/Frontend/src/components/songComponents/SongListing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import SongContainer from './SongContainer';
+import apiRequest from '../../requests/apiRequest';
 
 const SongListing = () => {
     const [searchValue, setSearchValue] = useState("");
--- Badcamp/Frontend/src/index.jsx
+++ Badcamp/Frontend/src/index.jsx
@@ -17,8 +17,8 @@ root.render(
         <Route path='/' element={<App />}>
           <Route index element={<Home />} element={<SongListing />}></Route>
           <Route path='SongListing' element={<SongListing />}></Route>
-          <Route exact path='events' element={<EventPage />}></Route>
-          <Route exact path='artistpage' element={<ArtistPage artistId={1}/>}></Route>
+          <Route exact path='/events' element={<EventPage />}></Route>
+          <Route exact path='/artistpage' element={<ArtistPage artistId={1}/>}></Route>
           <Route path='ArtistListing' element={<ArtistListing />}></Route>
         </Route>
       </Routes>
