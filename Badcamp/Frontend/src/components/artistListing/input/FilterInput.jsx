7e6b108a4f15c7e7dc144829c9a9df3a1837b609
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-15 16:08:56 +0200

    add FilterInput.jsx

---
+++ Badcamp/Frontend/src/components/artistListing/input/FilterInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+
+const FilterInput = ({filterValue,setFilterValue,genres}) => {
+    const handleSelect = (e) =>{
+        setFilterValue(e)
+        console.log(e)
+    }
+
+  return (
+    <DropdownButton
+    title={!filterValue ? "Genres" : filterValue}
+    id="dropdown-menu-align-right"
+    onSelect={(e) => handleSelect(e)}
+    className='m-0'
+      >
+        {genres.map((genre, index) =>(
+            <Dropdown.Item key={index} eventKey={genre}>{genre}</Dropdown.Item>
+        ))}
+        <Dropdown.Item eventKey=''>none</Dropdown.Item>
+
+    </DropdownButton>
+  )
+}
+
+export default FilterInput
