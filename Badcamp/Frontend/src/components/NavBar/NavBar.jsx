ada9e76c696675ae561e9280f2c15644204d475f
mBartal88 <miklos.bartal@gmail.com>
2022-09-15 15:36:21 +0200

    adding image to navbar

--- Badcamp/Frontend/src/components/NavBar/NavBar.jsx
+++ Badcamp/Frontend/src/components/NavBar/NavBar.jsx
@@ -5,14 +5,15 @@ const NavBar = () => {
 
     return (
         <nav className="Nav">
-            <img className="logo" src="./badcamp_logo.png"></img>
+            <img className="logo" src="./badcamp_logo2.png"></img>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Login</Link></li>
+                <li><Link to="login">Login</Link></li>
                 <li><Link to="/">Artists</Link></li>
-                <li><Link to="/">Users</Link></li>
+                <li><Link to="user/1">Users</Link></li>
                 <li><Link to="/">Register</Link></li>
-                <li><Link to="/">Logout</Link></li>
+                <li><Link to="register">Logout</Link></li>
+                <li><Link to="events">Events</Link></li>
             </ul>
         </nav>
     );
