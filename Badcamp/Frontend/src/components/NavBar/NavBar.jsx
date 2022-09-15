21719c06c3c2e7b773f092ae4b102310cf15282d
mBartal88 <miklos.bartal@gmail.com>
2022-09-15 11:00:13 +0200

    adding link to userpage on navbar

--- Badcamp/Frontend/src/components/NavBar/NavBar.jsx
+++ Badcamp/Frontend/src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/">Login</Link></li>
                 <li><Link to="/">Artists</Link></li>
-                <li><Link to="/">Users</Link></li>
+                <li><Link to="user">Users</Link></li>
                 <li><Link to="/">Register</Link></li>
                 <li><Link to="/">Logout</Link></li>
             </ul>
