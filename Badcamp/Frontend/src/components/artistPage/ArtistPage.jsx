69e025f904f4ac70c7268205f611ac16761d2427
KisLorand <kislorand270@gmail.com>
2022-09-15 08:37:18 +0200

    fix fetching, add loading message

--- Badcamp/Frontend/src/components/artistPage/ArtistPage.jsx
+++ Badcamp/Frontend/src/components/artistPage/ArtistPage.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useState, useEffect, useContext } from 'react';
 import ArtistName from './ArtistName';
 import ArtistDescription from './ArtistDescription';
+import apiRequest from '../../requests/apiRequest';
+import { RequestContext } from '../../requests/requestContext';
 
-const ArtistPage = () => {
+const ArtistPage = (props) => {
+  const [artist, setArtist] = useState(null); 
+  const url = "http://localhost:3000/artist";
+  useEffect(() => {
+    apiRequest(url, [artist, setArtist], RequestContext);
+  }, [props.artistId]);
+  //const {asd, setAsd} = useContext(RequestContext);
+
   return (
+    artist === null ? 
+    <p>"Loading..."</p> : 
     <div>
-      <ArtistName />
-      <ArtistDescription />
+        <ArtistName artistName={artist[0].name}  />
+        <ArtistDescription artistDesc={artist[0].description}/>
     </div>
   )
 };
