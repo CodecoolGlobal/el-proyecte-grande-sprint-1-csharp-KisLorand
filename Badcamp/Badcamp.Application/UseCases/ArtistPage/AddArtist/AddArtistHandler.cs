a692536fe068e65ed404fbb230631f574e09318e
KisLorand <kislorand270@gmail.com>
2022-09-27 13:15:45 +0200

    add AddArtistHandler

---
+++ Badcamp/Badcamp.Application/UseCases/ArtistPage/AddArtist/AddArtistHandler.cs
@@ -0,0 +1,40 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.ArtistPage.AddArtist
+{
+	public class AddArtistHandler : IRequestHandler<AddArtistRequest, Response<IReadOnlyList<ArtistModel>>>
+	{
+		private ArtistStorage _storage;
+		public AddArtistHandler(ArtistStorage storage)
+		{
+			_storage = storage;
+		}
+
+		public Response<IReadOnlyList<ArtistModel>> Handle(AddArtistRequest message)
+		{
+			IReadOnlyList<ArtistModel> artists;
+			try
+			{
+				_storage.AddArtist(message.Artist);
+				artists = _storage.GetArtists().ToList().AsReadOnly();
+				if (artists == null)
+				{
+					return Response.Fail<IReadOnlyList<ArtistModel>>("Artists list not found");
+				}
+				return Response.Ok(artists);
+			}
+			catch (Exception e)
+			{
+				return Response.Fail<IReadOnlyList<ArtistModel>>(e.Message);
+
+			}
+		}
+	
+	}
+}
