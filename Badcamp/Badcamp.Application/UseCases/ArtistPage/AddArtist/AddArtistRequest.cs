2414d5195a45cfce579a942ecb51413acf169a41
KisLorand <kislorand270@gmail.com>
2022-09-27 13:16:08 +0200

    add AddArtistRequest

---
+++ Badcamp/Badcamp.Application/UseCases/ArtistPage/AddArtist/AddArtistRequest.cs
@@ -0,0 +1,15 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.ArtistPage.AddArtist
+{
+	public class AddArtistRequest : IRequest<Response>
+	{
+		public ArtistModel Artist;
+	}
+}
