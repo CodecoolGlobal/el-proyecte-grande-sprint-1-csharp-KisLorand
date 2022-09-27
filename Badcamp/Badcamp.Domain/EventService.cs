603470cba297889954c70055c85ee3ef3b6e2b55
Imreszab <szabimi12@gmail.com>
2022-09-27 13:07:24 +0200

    add GetAllEvents and GetEventByArtist changed to clean architecture

---
+++ Badcamp/Badcamp.Domain/EventService.cs
@@ -0,0 +1,79 @@
+﻿using System.Reflection;
+using Badcamp.Models;
+
+namespace Badcamp.Services
+{
+    public class EventService
+    {
+        public List<Event> Storage;
+
+        public EventService()
+        {
+            Storage = new List<Event>();
+            Event newEvent = new Event(0, 0, "Concert", "Let's meet there!!", 70);
+            Event newEvent2 = new Event(1, 2, "Kázmér is giving free hugs", "Let's meet there!!", 200);
+            Storage.Add(newEvent);
+            Storage.Add(newEvent2);
+        }
+        public IReadOnlyList<Event> GetAllEvents()
+        {
+            return Storage.AsReadOnly();
+        }
+
+        public Event CreateEvent(int artistId, Event newEvent)
+        {
+            int id = 0;
+            if (Storage.Count > 0)
+            {
+                id = Storage.Last().Id +1;
+            }
+            newEvent.Id = id;
+            newEvent.ArtistId = artistId;
+
+            
+            Storage.Add(newEvent);
+            return newEvent;
+        }
+
+        public IReadOnlyList<Event> GetEventByArtist(int artistId)
+        {
+            List<Event> eventsByArtist = new List<Event>();
+            foreach (Event @event in Storage)
+            {
+                if (@event.ArtistId == artistId)
+                {
+                    eventsByArtist.Add(@event);
+                }
+            }
+            return eventsByArtist.AsReadOnly();
+        }
+
+        public IReadOnlyList<Event> DeleteEvent(int artistId, int eventId)
+        {
+            
+            for (int i=0; i<=Storage.Count; i++)
+            {
+                if (Storage[i].Id == eventId)
+                {
+                Storage.Remove(Storage[i]);
+                    break;
+                }
+            }
+
+            return GetEventByArtist(artistId);
+        }
+
+        public IReadOnlyList<Event> UpdateEvent(int artistId, int eventId, Event eventUpdate)
+        {
+            foreach(Event @event in Storage)
+            {
+                if(@event.Id == eventId)
+                {
+                    @event.Title = eventUpdate.Title;
+                    @event.Description = eventUpdate.Description;
+                }
+            }
+            return GetEventByArtist(artistId);
+        }
+    }
+}
--- Badcamp/Badcamp/Controllers/EventController.cs
+++ Badcamp/Badcamp/Controllers/EventController.cs
@@ -1,4 +1,5 @@
-﻿using Badcamp.Models;
+﻿using Badcamp.Application.UseCases.EventCases;
+using Badcamp.Models;
 using Badcamp.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,9 +12,12 @@ namespace Badcamp.Controllers
     public class EventController : ControllerBase
     {
         private EventService _eventService;
-        public EventController(EventService service)
+        private ILogger<EventController> _logger;
+
+        public EventController(EventService service, ILogger<EventController> logger)
         {
             _eventService = service;
+            _logger = logger;
         }
 
         [Route("{artistId}/Create")]
@@ -28,30 +32,46 @@ namespace Badcamp.Controllers
         [HttpGet]
         public ActionResult<List<Event>> GetEventsByArtist(int artistId)
         {
-            List<Event> eventList = _eventService.GetEventByArtist(artistId);
-            return Ok(eventList);
+            var request = new GetEventByArtistRequest { ArtistId = artistId };
+            var handler = new GetEventByArtistHandler(_eventService);
+            var response = handler.Handle(request);
+            if (response.Failure)
+            {
+                _logger.LogError(response.Error);
+                return BadRequest(response.Error);
+            }
+            _logger.LogInformation("user received");
+            return Ok(response.Value);
         }
 
         [Route("GetEvents")]
         [HttpGet]
         public ActionResult<List<Event>> GetEvents()
         {
-            List<Event> eventList = _eventService.Storage;
-            return Ok(eventList);
+            var request = new GetAllEventsRequest {};
+            var handler = new GetAllEventsHandler(_eventService);
+            var response = handler.Handle(request);
+            if (response.Failure)
+            {
+                _logger.LogError(response.Error);
+                return BadRequest(response.Error);
+            }
+            _logger.LogInformation("user received");
+            return Ok(response.Value);
         }
 
         [Route("{artistId}/DeleteEvent/{eventId}")]
         [HttpDelete]
-        public ActionResult<List<Event>> DeleteEvent(int artistId, int eventId)
+        public ActionResult<IReadOnlyList<Event>> DeleteEvent(int artistId, int eventId)
         {
-            List<Event> events = _eventService.DeleteEvent(artistId, eventId);
+            IReadOnlyList<Event> events = _eventService.DeleteEvent(artistId, eventId);
             return Ok(events);
         }
         [Route("{artistId}/UpdateEvent/{eventId}")]
         [HttpPut]
-        public ActionResult<List<Event>> UpdateEvent(int artistId, int eventId, [FromBody] Event eventUpdate)
+        public ActionResult<IReadOnlyList<Event>> UpdateEvent(int artistId, int eventId, [FromBody] Event eventUpdate)
         {
-            List<Event> events = _eventService.UpdateEvent(artistId, eventId, eventUpdate);
+            IReadOnlyList<Event> events = _eventService.UpdateEvent(artistId, eventId, eventUpdate);
             return Ok(events);
         }
     }
