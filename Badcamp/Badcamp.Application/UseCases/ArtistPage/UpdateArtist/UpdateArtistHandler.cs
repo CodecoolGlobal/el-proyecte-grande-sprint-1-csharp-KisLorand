775a160c8614fd2d92f04898dd440f99ef13ea44
KisLorand <kislorand270@gmail.com>
2022-09-27 13:24:18 +0200

    add UpdateArtistHandler base

---
+++ Badcamp/Badcamp.Application/UseCases/ArtistPage/UpdateArtist/UpdateArtistHandler.cs
@@ -0,0 +1,24 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.ArtistPage.UpdateArtist
+{
+	public class UpdateArtistHandler : IRequestHandler<UpdateArtistRequest, Response<IReadOnlyList<ArtistModel>>>>
+	{
+		private ArtistStorage _storage;
+		public UpdateArtistHandler(ArtistStorage storage)
+		{
+			_storage = storage;
+		}
+
+		public Response<IReadOnlyList<ArtistModel>> Handle(UpdateArtistRequest message)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
