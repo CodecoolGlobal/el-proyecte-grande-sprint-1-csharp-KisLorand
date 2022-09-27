df00ffecc3cb2fe54b66cc2b2768e69de47eb1e2
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-27 14:34:21 +0200

    add GetAllArtistsHandlerRequest.cs

---
+++ Badcamp/Badcamp.Application/UseCases/ArtistGalleryCases/GetAllArtistsHandlerRequest.cs
@@ -0,0 +1,14 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.ArtistGalleryCases
+{
+    public class GetAllArtistsHandlerRequest : IRequest<Response<ArtistModel>>
+    {
+    }
+}
