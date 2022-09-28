6f0fceee4ad421c84d020117a30d483aef92cd2e
Imreszab <szabimi12@gmail.com>
2022-09-28 12:05:10 +0200

    change entities for EF Core usage

--- Badcamp/Badcamp.Domain/Entities/Event.cs
+++ Badcamp/Badcamp.Domain/Entities/Event.cs
@@ -2,26 +2,12 @@
 {
     public class Event
     {
-        public int Id { get; set; }
-        public int ArtistId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public long Id { get; set; }
+        public ArtistModel Artist { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
         public int Upvote { get; set; }
 
-        public Event(int id, int artistId, string title, string description, int upvote)
-        {
-            Id = id;
-            ArtistId = artistId;
-            Title = title;
-            Description = description;
-            Upvote = upvote;
-
-        }
-        public Event()
-        {
-            Title = string.Empty;
-            Description = string.Empty;
-        }
     }
 }
--- Badcamp/Badcamp.Domain/Entities/Song.cs
+++ Badcamp/Badcamp.Domain/Entities/Song.cs
@@ -2,24 +2,22 @@
 {
     public class Song
     {
-        public int Id { get; private set; }
-        public int ArtistId { get; private set; }
-        public string Title { get; private set; } = String.Empty;
-        public string AlbumTitle { get; private set; } = String.Empty;
-        public string Description { get; private set; } = String.Empty;
-        public string Lyrics { get; private set; } = String.Empty;
-        public string AudioSource { get; private set; } = String.Empty;
-        public IEnumerable<Genre>? Genres { get; private set; }
-        public Song(int id, int artistId, string title, string album, string description, string lyrics, string audiosource, IEnumerable<Genre> genres)
+        public long Id { get; set; }
+        public ArtistModel Artist { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string AlbumTitle { get;  set; } = String.Empty;
+        public string Description { get;  set; } = String.Empty;
+        public string Lyrics { get; set; } = String.Empty;
+        public string AudioSource { get; set; } = String.Empty;
+        public HashSet<Genre>? Genres { get; set; }
+        public Song(int id, string title, string album, string description, string lyrics, string audiosource, IEnumerable<Genre> genres)
         {   
             Id = id;
-            ArtistId = artistId;
             Title = title;
             AlbumTitle = album;
             Description = description;
             Lyrics = lyrics;
             AudioSource = audiosource;
-            Genres = genres;
         }
 
         public Song()
@@ -33,10 +31,7 @@
 
         public void UpdateSong(Song song)
         {
-            if (ArtistId != song.ArtistId)
-            {
-                ArtistId = song.ArtistId;
-            }
+            
             if (Title != song.Title)
             {
                 Title = song.Title;
