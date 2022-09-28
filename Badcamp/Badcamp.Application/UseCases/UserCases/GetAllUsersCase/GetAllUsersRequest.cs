67c470be6f7865d0603b350256d32d1b80207c05
mBartal88 <miklos.bartal@gmail.com>
2022-09-28 15:09:02 +0200

    adding GetAllUsers UseCase

---
+++ Badcamp/Badcamp.Application/UseCases/UserCases/GetAllUsersCase/GetAllUsersRequest.cs
@@ -0,0 +1,13 @@
+﻿using Badcamp.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.UserCases.GetAllUsersCase
+{
+    public class GetAllUsersRequest : IRequest<Response>
+    {
+    }
+}
