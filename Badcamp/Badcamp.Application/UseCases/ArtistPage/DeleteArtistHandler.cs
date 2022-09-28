e6fefbcd349939ba049a3a0d87ded182185f8591
KisLorand <kislorand270@gmail.com>
2022-09-28 08:33:48 +0200

    add DeleteArtistHandler

---
+++ Badcamp/Badcamp.Application/UseCases/ArtistPage/DeleteArtistHandler.cs
@@ -0,0 +1,38 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.ArtistPage
+{
+	public class DeleteArtistHandler : IRequestHandler<ArtistIdRequest, Response<ArtistModel>>
+	{
+		private ArtistStorage _storage;
+		public DeleteArtistHandler(ArtistStorage storage)
+		{
+			_storage = storage;
+		}
+
+		public Response<ArtistModel> Handle(ArtistIdRequest message)
+		{
+			ArtistModel artist;
+			try
+			{
+				artist = _storage.GetArtist(message.Id);
+				if (artist == null)
+				{
+					return Response.Fail<ArtistModel>("Artist not found");
+				}
+				//_storage.DeleteArtist(message.Id);
+				return Response.Ok(artist);
+			}
+			catch (Exception ex)
+			{
+				return Response.Fail<ArtistModel>(ex.Message);
+			}
+		}
+	}
+}
