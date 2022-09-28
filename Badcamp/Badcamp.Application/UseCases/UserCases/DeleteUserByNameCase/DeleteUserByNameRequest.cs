65e2f3b419bec4f324f14210f197de7b22700f10
mBartal88 <miklos.bartal@gmail.com>
2022-09-28 15:08:46 +0200

    adding DeleteUserByName UseCase

---
+++ Badcamp/Badcamp.Application/UseCases/UserCases/DeleteUserByNameCase/DeleteUserByNameRequest.cs
@@ -0,0 +1,14 @@
+﻿using Badcamp.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.UserCases.DeleteUserCase
+{
+    public class DeleteUserByNameRequest : IRequest<Response>
+    {
+        public string UserName { get; set; } = string.Empty;
+    }
+}
