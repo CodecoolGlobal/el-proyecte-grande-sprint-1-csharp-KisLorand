6c3fd51297e9a6b4b133403ef5c2cb0f4624d234
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-29 14:30:47 +0200

    add DeleteGenreHandler.cs

---
+++ Badcamp/Badcamp.Application/UseCases/GenreCases/DeleteGenre/DeleteGenreHandler.cs
@@ -0,0 +1,24 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.GenreCases.DeleteGenre
+{
+    internal class DeleteGenreHandler : IRequestHandler<DeleteGenreRequest, Response<Genre>>
+    {
+
+        private IBadcampContext _context;
+        public DeleteGenreHandler(IBadcampContext context)
+        {
+            _context = context;
+        }
+        public Response<Genre> Handle(DeleteGenreRequest message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
