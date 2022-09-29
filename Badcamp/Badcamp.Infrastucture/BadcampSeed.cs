e31e519d913e6d6327b445707dc3499404ef46c1
KisLorand <kislorand270@gmail.com>
2022-09-29 10:10:36 +0200

    add more records t o Seed data

--- Badcamp/Badcamp.Infrastucture/BadcampSeed.cs
+++ Badcamp/Badcamp.Infrastucture/BadcampSeed.cs
@@ -49,51 +49,21 @@ namespace Badcamp.Infrastucture
                         Password = "asdfg",
                         DateOfBirth = new DateTime(1985, 8, 20, 7, 0, 0),
                         FullName = "Huba"
+                    },
+                    new User
+                    {
+                        Username = "Béla",
+                        Password = "asdfg",
+                        DateOfBirth = new DateTime(1985, 8, 20, 7, 0, 0),
+                        FullName = "Bela Lugosy"
+                    },
+                    new User
+                    {
+                        Username = "IMI",
+                        Password = "imi",
+                        DateOfBirth = new DateTime(1990, 8, 20, 7, 0, 0),
+                        FullName = "Imre"
                     }
-                    );
-                context.SaveChanges();
-            }
-            if (!context.ArtistModels.Any())
-            {
-                context.ArtistModels.AddRange(
-                    new ArtistModel
-                    {
-                        Name = "KA-ZZ",
-                        User = context.Users.Where(user => user.Username == "Kázmér").FirstOrDefault(),
-                        Description = "",
-                        ProfilePicture = "/kazimir_3.png",
-                    },
-                    new ArtistModel
-                    {
-                        Name = "Hubba",
-                        User = context.Users.Where(user => user.Username == "Huba").FirstOrDefault(),
-                        Description = "",
-                        ProfilePicture = "/huba_and_kazimir.png",
-                        ArtistGenre =
-                        Events =
-                        Songs =
-                    },
-                    new ArtistModel
-                    {
-                        Name = "Gizi",
-                        User = context.Users.Where(user => user.Username == "Gisella").FirstOrDefault(),
-                        Description = "",
-                        ProfilePicture = "/gizella.png",
-                    },
-                    new ArtistModel
-                    {
-                        Name = "Billy Geen",
-                        User = context.Users.Where(user => user.Username == "Kázmér").FirstOrDefault(),
-                        Description = "",
-                        ProfilePicture = "",
-                    },
-                    new ArtistModel
-                    {
-                        Name = "Johny Cache",
-                        User = context.Users.Where(user => user.Username == "Kázmér").FirstOrDefault(),
-                        Description = "",
-                        ProfilePicture = "/gizella.png",
-                    },
                     );
                 context.SaveChanges();
             }
@@ -131,7 +101,52 @@ namespace Badcamp.Infrastucture
                     );
                 context.SaveChanges();
             }
-            if (!context.Songs.Any())
+            if (!context.Artists.Any())
+            {
+                context.Artists.AddRange(
+                    new ArtistModel
+                    {
+                        Name = "KA-ZZ",
+                        User = context.Users.Where(user => user.Username == "Kázmér").FirstOrDefault(),
+                        Description = "I don't want peace. I want Problems Allways.",
+                        ProfilePicture = "/kazimir_3.png",
+                    },
+                    new ArtistModel
+                    {
+                        Name = "Hubba",
+                        User = context.Users.Where(user => user.Username == "Huba").FirstOrDefault(),
+                        Description = "",
+                        ProfilePicture = "/huba_and_kazimir.png",
+                        ArtistGenre =
+                        Events =
+                        Songs =
+                    },
+                    new ArtistModel
+                    {
+                        Name = "Gizi",
+                        User = context.Users.Where(user => user.Username == "Gisella").FirstOrDefault(),
+                        Description = "",
+                        ProfilePicture = "/gizella.png",
+                    },
+                    new ArtistModel
+                    {
+                        Name = "Billy Geen",
+                        User = context.Users.Where(user => user.Username == "Béla").FirstOrDefault(),
+                        Description = "",
+                        ProfilePicture = "",
+                    },
+                    new ArtistModel
+                    {
+                        Name = "Johny Cache",
+                        User = context.Users.Where(user => user.Username == "Elek").FirstOrDefault(),
+                        Description = "",
+                        ProfilePicture = "/gizella.png",
+                    }
+                    );
+                context.SaveChanges();
+            }
+            if (!context.Songs.Any()
+                && !context.Artists.Any())
             {
                 context.Songs.AddRange(
                     new Song
@@ -145,6 +160,17 @@ namespace Badcamp.Infrastucture
                             context.Genres.Where(x => x.Name=="Hip-Hopp"),
                             context.Genres.Where(x => x.Name=="Pop"),
                         }
+                    },
+                    new Song
+                    {
+                        Title = "The Dream",
+                        AlbumTitle = "Derams",
+                        Description = "",
+                        Lyrics = "",
+                        AudioSource = "/public/music",
+                        Genres = new HashSet<Genre> {
+                            context.Genres.Where(x => x.Name=="Rock"),
+                        }
                     }
                     );
                 context.SaveChanges();
@@ -154,24 +180,24 @@ namespace Badcamp.Infrastucture
                 context.Events.AddRange(
                     new Event
                     {
-                        Artist = context.ArtistModels.Where(x => x.Name == "Hubba").FirstOrDefault(),
+                        Artist = context.Artists.Where(x => x.Name == "Hubba").FirstOrDefault(),
                         Title = "Meet up",
                         Description = "Meet me in person at 31/09/2022",
                         Upvote = 42
                     },
                     new Event
                     {
-                        Artist = context.ArtistModels.Where(x => x.Name == "Hubba").FirstOrDefault(),
+                        Artist = context.Artists.Where(x => x.Name == "Hubba").FirstOrDefault(),
                         Title = "Handshakes",
                         Description = "Shake it!!",
-                        Upvote = 42
+                        Upvote = 200
                     },
                     new Event
                     {
-                        Artist = context.ArtistModels.Where(x => x.Name == "KA-ZZ").FirstOrDefault(),
+                        Artist = context.Artists.Where(x => x.Name == "KA-ZZ").FirstOrDefault(),
                         Title = "Free Huggs",
                         Description = "Offering hugs for the low, low cost of free",
-                        Upvote = 42
+                        Upvote = 4
                     }
                     );
                 context.SaveChanges();
