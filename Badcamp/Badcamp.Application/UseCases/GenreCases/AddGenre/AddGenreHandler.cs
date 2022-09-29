dbbd44bb1e7c0d47220137cc6a3d8fcb5a41b461
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-29 14:29:16 +0200

    add Handler

--- Badcamp/Badcamp.Application/UseCases/GenreCases/AddGenre/AddGenreHandler.cs
+++ Badcamp/Badcamp.Application/UseCases/GenreCases/AddGenre/AddGenreHandler.cs
@@ -1,6 +1,5 @@
 ﻿using Badcamp.Application.Common;
 using Badcamp.Domain.Entities;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +8,31 @@ using System.Threading.Tasks;
 
 namespace Badcamp.Application.UseCases.GenreCases.AddGenre
 {
-    internal class AddGenreHandler : IRequestHandler<AddGenreRequest, Response<Genre>>
+    public class AddGenreHandler : IRequestHandler<AddGenreRequest, Response<Genre>>
     {
 
         private IBadcampContext _context;
 
-
-        private ILogger<AddGenreHandler> _logger;
-
-        public AddGenreHandler(IBadcampContext context, ILogger<AddGenreHandler> logger)
+        public AddGenreHandler(IBadcampContext context)
         {
             _context = context;
-            _logger = logger;
+
         }
 
         public Response<Genre> Handle(AddGenreRequest message)
         {
             var genre = new Genre() { Name = message.Name};
+            try
+            {
+                _context.Genres.Add(genre);
+                _context.SaveChanges();
+                return Response.Ok(genre);
+            }
+            catch (Exception e)
+            {
+
+                return Response.Fail<Genre>(e.Message);
+            }
         }
     }
 }
