f6eca4ed0b4f79851a7d7f0a5d6224bd080b0d56
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-09-29 13:21:36 +0200

    add inject logger & context

--- Badcamp/Badcamp.Application/UseCases/GenreCases/AddGenre/AddGenreHandler.cs
+++ Badcamp/Badcamp.Application/UseCases/GenreCases/AddGenre/AddGenreHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Badcamp.Application.Common;
+using Badcamp.Domain.Entities;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +9,23 @@ using System.Threading.Tasks;
 
 namespace Badcamp.Application.UseCases.GenreCases.AddGenre
 {
-    internal class AddGenreHandler
+    internal class AddGenreHandler : IRequestHandler<AddGenreRequest, Response<Genre>>
     {
 
         private IBadcampContext _context;
+
+
+        private ILogger<AddGenreHandler> _logger;
+
+        public AddGenreHandler(IBadcampContext context, ILogger<AddGenreHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public Response<Genre> Handle(AddGenreRequest message)
+        {
+            var genre = new Genre() { Name = message.Name};
+        }
     }
 }
