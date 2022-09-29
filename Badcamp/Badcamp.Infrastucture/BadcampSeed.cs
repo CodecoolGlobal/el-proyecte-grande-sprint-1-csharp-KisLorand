243142a3530492e8ffaf1445ff1cb37ef03f9f2b
KisLorand <kislorand270@gmail.com>
2022-09-29 10:48:50 +0200

    updating Seed data

--- Badcamp/Badcamp.Infrastucture/BadcampSeed.cs
+++ Badcamp/Badcamp.Infrastucture/BadcampSeed.cs
@@ -184,6 +184,120 @@ namespace Badcamp.Infrastucture
                             context.Genres.Where(x => x.Name=="Rock"),
                             context.Genres.Where(x => x.Name=="Techno")
                         }
+                    },
+                    new Song
+                    {
+                        Title = "Donkey Autopsy",
+                        AlbumTitle = "Big Boy Band",
+                        Description = "A song about suffering and struggling",
+                        Lyrics = @"Donkey Autopsy
+                                    Have You Met John?
+                                    Stand by Your Donkey
+                                    Good Donkey
+                                    Mammas Don't Let Your Babies Grow Up to Be Cats
+                                    The Number of your Donkey
+                                    Ring of Donkey
+                                    Rhythm of the Donkey
+                                    Nice Weather For Cats
+                                    PAfrica is Your Land
+                                    (I Can't Get No) Supercallifragisticexpialidocious Cats
+                                    Stairway to PAfrica
+                                    Born Supercallifragisticexpialidocious
+                                    We Shall Suffer
+                                    I Suffer
+                                    They Are Night Cats! They Have Come Back From PAfrica!! Ahhhh!
+                                    Donkey Deep, Cats High
+                                    Takin' the Donkey Train
+                                    Supercallifragisticexpialidocious Cats Forever
+                                    Donkey Fields Forever
+                                    Like a Supercallifragisticexpialidocious Donkey
+                                    This Is A Sight We Had One Day From Supercallifragisticexpialidocious PAfrica
+                                    Here Without John
+                                    The Donkey Where Your Heart Should Be
+                                    It Hurts To Shoot Cats From Your Donkey, But It's Necessary
+                                    Don't Suffer
+                                    John Eat My Supercallifragisticexpialidocious Cats in PAfrica
+                                    Donkey I Have Become
+                                    Supercallifragisticexpialidocious Blues
+                                    Cats Sound Better With You
+                                    Your Supercallifragisticexpialidocious Heart
+                                    Just Another a Supercallifragisticexpialidocious Donkey
+                                    When Supercallifragisticexpialidocious Cats Suffer
+                                    Let's Run Away to PAfrica and Swim With Cats
+                                    Amazing John
+                                    Ghost in My Donkey
+                                    She Thinks Donkey's Sexy
+                                    John's Waiting
+                                    Another Year of Cats
+                                    I Suffer in Your Arms
+                                    Goody Two Cats
+                                    Great Balls of Cats
+                                    Truly Madly Supercallifragisticexpialidocious
+                                    Give Me Your Cats
+                                    Straight Outta PAfrica
+                                    Bed of Cats
+                                    Yearning for Supercallifragisticexpialidocious Cats
+                                    It's the End Of PAfrica As We Know It (And I Feel Fine)
+                                    Four Supercallifragisticexpialidocious Cats
+                                    Hotel PAfrica
+                                    Smack My Donkey Up
+                                    Suffer, Suffer, Suffer!
+                                    Baby, I Need Your Cats
+                                    Stand By John
+                                    Stairway to Cats
+                                    At Least Give Me My Cats Back, You Negligent Donkey!
+                                    There's A Good Reason Cats Are Numbered, John
+                                    Stand by Your Cats
+                                    Independent Donkey
+                                    Like a Donkey
+                                    Donkey Boogie
+                                    You've Lost That Supercallifragisticexpialidocious Donkey
+                                    Look John, are you going to Suffer With Me or Not?
+                                    My Name is John
+                                    Sweet Donkey O' Mine
+                                    You've Lost That Supercallifragisticexpialidocious Feeling
+                                    The Homecoming Queen's Got A Donkey
+                                    Suffer - It is the Most Fun a Girl Can Have
+                                    The Girl From PAfrica
+                                    I plead Donkey
+                                    Every Donkey You Take
+                                    Many Pieces Of Large Fuzzy Cats Gathered Together At PAfrica And Grooving On A Donkey
+                                    Stairway to Donkey
+                                    Suffer? I Jolly Well Won't Suffer
+                                    Suffer Forever
+                                    Behind Supercallifragisticexpialidocious Cats
+                                    When Supercallifragisticexpialidocious Cats Cry
+                                    Like Supercallifragisticexpialidocious Cats
+                                    Supercallifragisticexpialidocious Rhapsody
+                                    Free Donkey
+                                    Welcome to Supercallifragisticexpialidocious PAfrica
+                                    Gonna Make You Suffer
+                                    Total Eclipse of the Donkey
+                                    Whole Lotta Cats
+                                    Supercallifragisticexpialidocious Donkey O' Mine
+                                    You Don't Send Me Supercallifragisticexpialidocious Cats Anymore
+                                    Bridge Over Supercallifragisticexpialidocious Cats
+                                    Livin' on a Donkey
+                                    House of the Supercallifragisticexpialidocious Donkey
+                                    My Donkey Sounds Better With You
+                                    Early Morning Suffer
+                                    Smells Like Supercallifragisticexpialidocious Cats
+                                    Early Morning Donkey
+                                    You Think I Ain't Worth A Donkey But I Feel Like A Million Cats
+                                    Another Donkey in the Wall
+                                    Careful With That Donkey
+                                    Hey John
+                                    My Donkey Wants To Kill Your Mama
+                                    Where Have All the Cats Gone?
+                                    In Donkey We Trust",
+                        AudioSource = "/public/music",
+                        Genres = new HashSet<Genre> {
+                            context.Genres.Where(x => x.Name=="Alter"),
+                            context.Genres.Where(x => x.Name=="Folk"),
+                            context.Genres.Where(x => x.Name=="Rock"),
+                            context.Genres.Where(x => x.Name=="Techno")
+                        }
+                    }
                     );
                 context.SaveChanges();
             }
