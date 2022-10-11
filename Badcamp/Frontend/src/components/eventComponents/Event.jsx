1cc74beabf4116f6b92e15e572cac334d5e1125c
Imreszab <szabimi12@gmail.com>
2022-10-11 14:48:37 +0200

    add fetch from db to frontend

--- Badcamp/Frontend/src/components/eventComponents/Event.jsx
+++ Badcamp/Frontend/src/components/eventComponents/Event.jsx
@@ -3,28 +3,36 @@ import Card from "react-bootstrap/Card";
 import "./EventCard.css";
 import { Link } from "react-router-dom";
 
-const Event = ({ item, artistName }) => {
-  return (
-    <Card className="eventCard border border-4 w-50" style={{ width: "18rem" }}>
-      <Card.Header>
-        <Card.Title>
-          {item.title}
-          <Card.Subtitle className="mb-2 text-muted text-end">
-            <Link to="/artistpage" style={{ textDecoration: "none" }}>
-              {artistName}
-            </Link>
-          </Card.Subtitle>
-        </Card.Title>
-      </Card.Header>
-      <Card.Body>
-        <Card.Text>{item.description}</Card.Text>
-        <div className="upVote">
-          {item.upVote}{" "}
-          <img src="/upvote-png.png" alt="image" className="upVote-img" />
-        </div>
-      </Card.Body>
-    </Card>
-  );
+const Event = ({ item, handleUpvote }) => {
+	return (
+		<Card className="eventCard border border-4 w-50" style={{ width: "18rem" }}>
+			<Card.Header>
+				<Card.Title>
+					{item.title}
+					<Card.Subtitle className="mb-2 text-muted text-end">
+						<Link
+							to={`/artistpage/${item.artist.id}`}
+							style={{ textDecoration: "none" }}
+						>
+							{item.artist.name}
+						</Link>
+					</Card.Subtitle>
+				</Card.Title>
+			</Card.Header>
+			<Card.Body>
+				<Card.Text>{item.description}</Card.Text>
+				<div className="upVote">
+					{item.upvote}{" "}
+					<img
+						src="/upvote-png.png"
+						alt="image"
+						className="upvote-img"
+						onClick={() => handleUpvote(item)}
+					/>
+				</div>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default Event;
--- Badcamp/Frontend/src/components/eventComponents/EventPage.jsx
+++ Badcamp/Frontend/src/components/eventComponents/EventPage.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import Event from './Event';
-import { useState, useEffect, useContext } from 'react';
-import apiRequest from '../../requests/apiRequest';
-
-
+import React from "react";
+import Event from "./Event";
+import { useState, useEffect, useContext } from "react";
+import apiRequest from "../../requests/apiRequest";
 
 const EventPage = (props) => {
-    const [events, setEvents] = useState(null)
-    const [artist, setArtist] = useState(null); 
-    const url1 = "http://localhost:3000/events";
-    const url2 = "http://localhost:3000/artists";
-    useEffect(() => {
-        apiRequest(url1, [events, setEvents]);
-        apiRequest(url2, [artist, setArtist]);
-      }, []);
-      
-      console.log(artist)
-      console.log(events)
-  return (
-    (events === null || artist === null) ? 
-    <p>"Loading..."</p> : 
-    <div>
-        {events.map((item) => {
-                return(
-                    <Event 
-                        key={item.id}
-                        item={item}
-                        artistName ={artist.map((artist) => {return(artist.id === item.artistId ? artist.name : "")})}
+	const [events, setEvents] = useState(null);
+	const urlGet = "https://localhost:7151/api/Event/GetEvents";
+	const urlUpdate = "https://localhost:7151/api/Event/UpdateEvent";
+	useEffect(() => {
+		apiRequest(urlGet, [events, setEvents]);
+	}, []);
 
-                        />
-                        
-        )})}
-            
-      
-    </div>
-  )
-}
+	const handleUpvote = async (item) => {
+		item.upvote += 1;
+		let fd = new FormData();
+		for (let prop in item) {
+			fd.append(prop, JSON.stringify(item[prop]));
+		}
+		const requestOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: fd,
+		};
+		fetch(urlUpdate, requestOptions).then((response) => response.json());
+	};
 
-export default EventPage
+	console.log(events);
+	return events === null ? (
+		<p>"Loading..."</p>
+	) : (
+		<div>
+			{events.map((item) => {
+				return <Event key={item.id} item={item} handleUpvote={handleUpvote} />;
+			})}
+		</div>
+	);
+};
+
+export default EventPage;
