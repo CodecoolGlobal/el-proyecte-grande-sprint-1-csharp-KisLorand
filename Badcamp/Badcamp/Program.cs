ed092e85cbdc9ebb76a1d2473092f40ba34abc0e
mBartal88 <miklos.bartal@gmail.com>
2022-10-11 09:42:17 +0200

    adding Cors and Authentication

--- Badcamp/Badcamp/Program.cs
+++ Badcamp/Badcamp/Program.cs
@@ -1,17 +1,34 @@
 using Badcamp.Services.Interfaces;
-
 using Badcamp;
 using Badcamp.Services;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Badcamp.Models;
 using Badcamp.Application;
 using Badcamp.Infrastucture;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
 
 builder.Services.AddControllersWithViews().AddJsonOptions(option =>
 {
@@ -52,7 +69,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder
+   .AllowAnyHeader()
+   .AllowAnyMethod()
+   .SetIsOriginAllowed((host) => true)
+   .AllowCredentials());
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
