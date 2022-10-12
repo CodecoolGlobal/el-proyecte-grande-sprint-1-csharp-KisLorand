212b5f31260931ee56ba9ae3e42abbd0c9834e0b
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-10-12 15:53:31 +0200

    add CreatePlaylistRequest.cs (implemented)

---
+++ Badcamp/Badcamp.Application/UseCases/PlaylistCases/CreatePlaylist/CreatePlaylistRequest.cs
@@ -0,0 +1,14 @@
+﻿using Badcamp.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.PlaylistCases.CreatePlaylist
+{
+    internal class CreatePlaylistRequest : IRequest<Response>
+    {
+        public string Name { get; set; }
+    }
+}
