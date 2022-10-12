ff1b56292befa924cbaa2c42e6f45283489a01b7
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-10-12 15:54:59 +0200

    add DeletePLaylistRequest.cs (implemented)

---
+++ Badcamp/Badcamp.Application/UseCases/PlaylistCases/DeletePlaylist/DeletePLaylistRequest.cs
@@ -0,0 +1,14 @@
+﻿using Badcamp.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.PlaylistCases.DeletePlaylist
+{
+    internal class DeletePLaylistRequest : IRequest<Response>
+    {
+        public int PlaylistId { get; set; }
+    }
+}
