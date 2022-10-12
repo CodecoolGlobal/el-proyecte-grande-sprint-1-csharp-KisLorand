e6ddcfd9c5095f202733a723a81019ee30915f6c
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-10-12 15:52:40 +0200

    add CreatePlaylistHandler .cs(implemented)

---
+++ Badcamp/Badcamp.Application/UseCases/PlaylistCases/CreatePlaylist/CreatePlaylistHandler.cs
@@ -0,0 +1,36 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.PlaylistCases.CreatePlaylist
+{
+    internal class CreatePlaylistHandler : IRequestHandler<CreatePlaylistRequest, Response<Playlist>>
+    {
+
+        private IBadcampContext _context;
+        public CreatePlaylistHandler(IBadcampContext context)
+        {
+            _context = context;
+        }
+        public Response<Playlist> Handle(CreatePlaylistRequest message)
+        {
+            var playlist = new Playlist();
+            try
+            {
+                playlist.Name = message.Name;
+                _context.Playlists.Add(playlist);
+                _context.SaveChanges();
+                return Response.Ok(playlist);
+            }
+            catch (Exception e)
+            {
+
+                return Response.Fail<Playlist>(e.Message);
+            }
+        }
+    }
+}
