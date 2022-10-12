dd15a3ea6df9b2c5b1685aa0cf7859f8bd43db85
KisLorand <kislorand270@gmail.com>
2022-10-12 15:27:34 +0200

    add nullable types to Artist

--- Badcamp/Badcamp.Domain/Entities/Artist.cs
+++ Badcamp/Badcamp.Domain/Entities/Artist.cs
@@ -7,9 +7,9 @@
 		public User? User { get; set; }
 		public string Description { get; set; } = String.Empty;
 		public string ProfilePicture { get; set; } = String.Empty;
-        public HashSet<Genre> Genres { get; set; } = new HashSet<Genre>();
-		public HashSet<Event> Events { get; set; } = new HashSet<Event>();
-		public HashSet<Song> Songs { get; set; } = new HashSet<Song>();
+        public HashSet<Genre>? Genres { get; set; } = new HashSet<Genre>();
+		public HashSet<Event>? Events { get; set; } = new HashSet<Event>();
+		public HashSet<Song>? Songs { get; set; } = new HashSet<Song>();
 
 
 
