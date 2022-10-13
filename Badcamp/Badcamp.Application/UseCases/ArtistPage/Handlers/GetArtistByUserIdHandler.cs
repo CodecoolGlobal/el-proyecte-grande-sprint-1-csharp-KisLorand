2e81597d34db0c723551672d26a5cdb71a454628
KisLorand <kislorand270@gmail.com>
2022-10-13 16:39:19 +0200

    add GetArtistByUserIdHandler

---
+++ Badcamp/Badcamp.Application/UseCases/ArtistPage/Handlers/GetArtistByUserIdHandler.cs
@@ -0,0 +1,41 @@
+﻿using Badcamp.Application.Common;
+using Badcamp.Application.UseCases.ArtistPage.Requests;
+using Badcamp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badcamp.Application.UseCases.ArtistPage.Handlers
+{
+	public class GetArtistByUserIdHandler : IRequestHandler<ArtistIdRequest, Response<Artist>>
+	{
+		private IBadcampContext _context;
+		public GetArtistByUserIdHandler(IBadcampContext context)
+		{
+			_context = context;
+		}
+
+
+		public Response<Artist> Handle(ArtistIdRequest message)
+		{
+			Artist artist;
+			try
+			{
+				artist = _context.Artists.Include(x => x.User).Where(x => x.User.Id == message.Id).FirstOrDefault();
+				if (artist == null)
+				{
+					return Response.Fail<Artist>("Artists list not found");
+				}
+				return Response.Ok(artist);
+			}
+			catch (Exception e)
+			{
+				return Response.Fail<Artist>(e.Message);
+
+			}
+		}
+	}
+}
