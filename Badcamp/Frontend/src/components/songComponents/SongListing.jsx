b1bb56c8c3affbb58d143f4254b23698630b2303
CaineCorso <danielz.boros@gmail.com>
2022-10-13 13:35:13 +0200

    cors debug test

--- Badcamp/Frontend/src/components/songComponents/SongListing.jsx
+++ Badcamp/Frontend/src/components/songComponents/SongListing.jsx
@@ -1,31 +1,23 @@
-import React from 'react'
-import { useState, useEffect, useContext } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import SongContainer from './SongContainer';
 import apiRequest from '../../requests/apiRequest';
 
 const SongListing = (props) => {
     const [searchValue, setSearchValue] = useState("");
     const [songs, setSongs] = useState(null)
-    const [artist, setArtists] = useState(null);
-    const url1 = process.env.REACT_APP_BASE_URL + "api/Song/getall";
-    //const url1 = "https://localhost:7151/api/Song/getall";
+    const getAll = process.env.REACT_APP_BASE_URL + "api/Song/getall";
     useEffect(() => {
-        apiRequest(url1, [songs, setSongs]);
+        apiRequest(getAll, [songs, setSongs]);
     }, []);
 
-    const test = () => {
-        console.log(songs)
-    }
-
     return (
-        { test() }
         (songs === null) ? <p> Loading Songs... </p> :
         <div>
             <SongContainer
-                songs={songs.filter((song) =>
+                songs={songs.map((song) =>
                     song.title.toLowerCase().includes(searchValue.toLocaleLowerCase())
                 )}
-                artists={setArtists}
                 setSongs={setSongs}
                 searchValue={searchValue}
                 setSearchValue={setSearchValue}
