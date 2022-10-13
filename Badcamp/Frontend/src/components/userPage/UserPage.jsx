d801727f01beaf2bcb156a3df93095bf3fd92101
mBartal88 <miklos.bartal@gmail.com>
2022-10-13 15:52:21 +0200

    adding unauthorized and edituser component

--- Badcamp/Frontend/src/components/userPage/UserPage.jsx
+++ Badcamp/Frontend/src/components/userPage/UserPage.jsx
@@ -2,13 +2,17 @@ import CardContainer from './CardContainer';
 import { useState, useEffect } from 'react';
 import './UserPage.css';
 import { useParams } from 'react-router-dom';
+import Unauthorized from '../Unauthorized';
+import EditUser from './EditUser';
 
 const UserPage = () => {
 
     const { id } = useParams();
-    const apiUrl = "http://localhost:3000/users";
+    const user = JSON.parse(localStorage.getItem('user'));
+    const apiUrl = "https://localhost:7151/GetUsers";
     const [items, setItems] = useState([]);
     const [fetchError, setFetchError] = useState(null);
+    const [state, setState] = useState(null);
 
     useEffect(() => {
         const fetchItems = async () => {
@@ -27,15 +31,27 @@ const UserPage = () => {
     }, []);
 
     return (
-        <div className="UserPage">
+        !state ? (user && user.userId === id
+        ?<div className="UserPage">
             <h1>My profile</h1>
             {items.filter(items => (items.id).toString() === id).map(user => (
                 <CardContainer
                     key={user.id}
                     user={user}
+                    setState={setState}
                 />
             ))}
         </div>
+        : <Unauthorized/>)
+        : <>{items.filter(items => (items.id).toString() === id).map(user => (
+            <EditUser
+                key={user.id}
+                userUpdate={user}
+                setState={setState}
+            />
+        ))} 
+        </>
+        
     );
 }
 
