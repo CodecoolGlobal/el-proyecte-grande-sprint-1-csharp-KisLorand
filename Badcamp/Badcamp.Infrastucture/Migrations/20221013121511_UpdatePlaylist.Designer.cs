1a774d0e7e24faf76abd401276f6c965b8676948
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-10-13 15:21:03 +0200

    update migration

--- Badcamp/Badcamp.Infrastucture/Migrations/20221013121511_UpdatePlaylist.Designer.cs
+++ Badcamp/Badcamp.Infrastucture/Migrations/20221013121511_UpdatePlaylist.Designer.cs
@@ -4,6 +4,7 @@ using Badcamp.Infrastucture;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Badcamp.Infrastucture.Migrations
 {
     [DbContext(typeof(BadcampContext))]
-    partial class BadcampContextModelSnapshot : ModelSnapshot
+    [Migration("20221013121511_UpdatePlaylist")]
+    partial class UpdatePlaylist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Badcamp/Badcamp.Infrastucture/Migrations/20221013121511_UpdatePlaylist.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Badcamp.Infrastucture.Migrations
+{
+    public partial class UpdatePlaylist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Artists_ArtistId",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlaylistId",
+                table: "Songs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ArtistId",
+                table: "Events",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Playlists_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_PlaylistId",
+                table: "Songs",
+                column: "PlaylistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Playlists_UserId",
+                table: "Playlists",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Artists_ArtistId",
+                table: "Events",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Playlists_PlaylistId",
+                table: "Songs",
+                column: "PlaylistId",
+                principalTable: "Playlists",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Artists_ArtistId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Playlists_PlaylistId",
+                table: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Playlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Songs_PlaylistId",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "PlaylistId",
+                table: "Songs");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ArtistId",
+                table: "Events",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Artists_ArtistId",
+                table: "Events",
+                column: "ArtistId",
+                principalTable: "Artists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
