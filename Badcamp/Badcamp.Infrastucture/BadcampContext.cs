22eeaa30f3cb2ca7850274961d217498e5f3877a
NyitraiBalzs <nyitrai.bazs@gmail.com>
2022-10-13 15:19:50 +0200

    delete comments

--- Badcamp/Badcamp.Infrastucture/BadcampContext.cs
+++ Badcamp/Badcamp.Infrastucture/BadcampContext.cs
@@ -9,24 +9,15 @@ namespace Badcamp.Infrastucture
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Artist> Artists { get; set; }
-        public DbSet<Genre> Genres  { get; set; }
+        public DbSet<Genre> Genres { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<Song> Songs { get; set; }
         public DbSet<Playlist> Playlists { get; set; }
         public BadcampContext(DbContextOptions<BadcampContext> options)
-        :base(options)
+        : base(options)
         {
-           
-        }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder
-        //        .Entity<Playlist>()
-        //        .HasMany(p => p.Songs)
-        //        .WithOne(p => p.Playlists)
-        //        .OnDelete(DeleteBehavior.ClientCascade);
-        //}
+        }
 
         void IBadcampContext.SaveChanges()
         {
