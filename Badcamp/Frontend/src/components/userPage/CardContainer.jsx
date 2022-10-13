9df7f70686902f80f9bb29abd7dbf47d55360e7f
mBartal88 <miklos.bartal@gmail.com>
2022-10-13 15:52:40 +0200

    propping setState variable

--- Badcamp/Frontend/src/components/userPage/CardContainer.jsx
+++ Badcamp/Frontend/src/components/userPage/CardContainer.jsx
@@ -3,13 +3,14 @@ import PlaylistCard from './PlaylistCard';
 import Card from 'react-bootstrap/Card';
 
 
-const CardContainer = ({ user }) => {
+const CardContainer = ({ user, setState }) => {
 
     return (
         <div>
             <Card style={{ width: '18rem', border: 'solid 2px black' }} bg="light" className="CardContainer">
                 <CardItem
                     user={user}
+                    setState={setState}
                 />
             </Card>
             <Card style={{ width: '18rem', border: 'solid 2px black' }} bg="dark" text="white" className="CardContainer">
